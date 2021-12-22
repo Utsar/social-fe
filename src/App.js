12e6dbe852e9b9b97b6c9fb74688fa9da2708754
Kristian Utsar <utsaru@gmail.com>
2021-12-22 12:25:46 +0000

    components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Home from "./pages/home/Home";
+import { Person } from "@material-ui/icons";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
