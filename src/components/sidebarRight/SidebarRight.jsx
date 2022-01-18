14f7da7e8cbb77de02ffae6af40a21449c411e26
Kristian Utsar <utsaru@gmail.com>
2022-01-18 13:54:34 +0000

    ads rightbar

--- src/components/sidebarRight/SidebarRight.jsx
+++ src/components/sidebarRight/SidebarRight.jsx
@@ -4,7 +4,31 @@ import "./sidebarRight.css";
 const sidebarRight = () => {
   return (
     <>
-      <div className="sidebarRight">Right</div>
+      <div className="sidebarRight">
+        <div className="rightbarWrapper">
+          <div className="birthdayContainer">
+            <img src="/Assets/gift.png" alt="" className="birthdayImage" />
+            <span className="birthdayText">
+              <b>Lala</b> and <b>3 other frineds </b>have bday today
+            </span>
+          </div>
+          <img src="/Assets/ad.jpg" alt="" className="rightbarAd" />
+          <h4 className="rightbarTitle">Online Friends</h4>
+          <ul className="rightbarFriendList">
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImageContainer">
+                <img
+                  src="/Assets/profiles/profile4.jpg"
+                  alt=""
+                  className="rightbarProfileImage"
+                />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarFriendName">Lala</span>
+            </li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 };
