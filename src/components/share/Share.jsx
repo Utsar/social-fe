c745f142104f85aaf7203296291a1edeeaf8eb6e
Kristian Utsar <utsaru@gmail.com>
2022-01-18 12:08:46 +0000

    share options

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,27 +1,39 @@
 import "./share.css";
-import { PermMedia } from "@material-ui/icons";
+import { PermMedia, Label, Room, EmojiEmotions } from "@material-ui/icons";
 
 const Share = () => {
   return (
     <div className="share">
-      hello i'm share
       <div className="shareWrapper">
         <div className="shareTop">
           <img
             className="shareProfileImage"
-            src="/public/Assets/profiles/profile2.jpg"
+            src="/Assets/profiles/profile2.jpg"
             alt="profile"
           />
-          <input placeholder="Whats's in your mind?" className="shareinput" />
+          <input placeholder="Whats's in your mind?" className="shareInput" />
         </div>
         <hr className="shareHr" />
         <div className="shareBottom">
           <div className="shareOptions">
             <div className="shareOption">
-              <PermMedia className="" />
+              <PermMedia htmlColor="tomato" className="shareIcon" />
               <span className="shareOptionText">Photo or Video</span>
             </div>
+            <div className="shareOption">
+              <Label htmlColor="blue" className="shareIcon" />
+              <span className="shareOptionText">Tag</span>
+            </div>
+            <div className="shareOption">
+              <Room htmlColor="green" className="shareIcon" />
+              <span className="shareOptionText">Location</span>
+            </div>
+            <div className="shareOption">
+              <EmojiEmotions htmlColor="goldenrod" className="shareIcon" />
+              <span className="shareOptionText">Feelings</span>
+            </div>
           </div>
+          <div className="shareButton">Share</div>
         </div>
       </div>
     </div>
