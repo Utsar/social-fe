1af5a476181c4efc15a0b192db16dc3f1851f2ea
Kristian Utsar <utsaru@gmail.com>
2022-01-21 18:44:28 +0000

    adds profile page and right sidebar adjustments

--- src/components/sidebarRight/SidebarRight.jsx
+++ src/components/sidebarRight/SidebarRight.jsx
@@ -3,24 +3,88 @@ import "./sidebarRight.css";
 import { Users } from "../../dummyData";
 import Online from "../online/Online";
 
-const sidebarRight = () => {
+const sidebarRight = ({ profile }) => {
+  const HomeSidebarRight = () => {
+    return (
+      <>
+        <div className="birthdayContainer">
+          <img src="/Assets/gift.png" alt="" className="birthdayImage" />
+          <span className="birthdayText">
+            <b>Lala</b> and <b>3 other frineds </b>have bday today
+          </span>
+        </div>
+        <img src="/Assets/ad.jpg" alt="" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          {Users.map((user) => (
+            <Online key={user.id} users={user} />
+          ))}
+        </ul>
+      </>
+    );
+  };
+
+  const ProfileSidebarRight = () => {
+    return (
+      <>
+        <h4 className="rightbarTitle">User info</h4>
+        <div className="rightbarInfo">
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">City</span>
+            <span className="rightbarInfoValue">London</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">From:</span>
+            <span className="rightbarInfoValue">Estonia</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">Relationship</span>
+            <span className="rightbarInfoValue">Married</span>
+          </div>
+          <h4 className="rightbarTitle">User info</h4>
+          <div className="rightbarFollowers">
+            <div className="rightbarFollower">
+              <img
+                src="/Assets/profiles/profile4.jpg"
+                alt=""
+                className="rightbarFollowerImage"
+              />
+              <span className="rightbarFollowerName">Cider</span>
+            </div>
+            <div className="rightbarFollower">
+              <img
+                src="/Assets/profiles/profile3.jpg"
+                alt=""
+                className="rightbarFollowerImage"
+              />
+              <span className="rightbarFollowerName">Cider</span>
+            </div>
+            <div className="rightbarFollower">
+              <img
+                src="/Assets/profiles/profile2.jpg"
+                alt=""
+                className="rightbarFollowerImage"
+              />
+              <span className="rightbarFollowerName">Cider</span>
+            </div>
+            <div className="rightbarFollower">
+              <img
+                src="/Assets/profiles/profile1.jpg"
+                alt=""
+                className="rightbarFollowerImage"
+              />
+              <span className="rightbarFollowerName">Cider</span>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
   return (
     <>
       <div className="sidebarRight">
         <div className="rightbarWrapper">
-          <div className="birthdayContainer">
-            <img src="/Assets/gift.png" alt="" className="birthdayImage" />
-            <span className="birthdayText">
-              <b>Lala</b> and <b>3 other frineds </b>have bday today
-            </span>
-          </div>
-          <img src="/Assets/ad.jpg" alt="" className="rightbarAd" />
-          <h4 className="rightbarTitle">Online Friends</h4>
-          <ul className="rightbarFriendList">
-            {Users.map((user) => (
-              <Online key={user.id} users={user} />
-            ))}
-          </ul>
+          <ProfileSidebarRight />
         </div>
       </div>
     </>
--- src/pages/profilePage/Profile.jsx
+++ src/pages/profilePage/Profile.jsx
@@ -1,7 +1,42 @@
 import "./profile.css";
+import Feed from "../../components/feed/Feed.jsx";
+import SidebarLeft from "../../components/sidebarLeft/SidebarLeft.jsx";
+import SidebarRight from "../../components/sidebarRight/SidebarRight.jsx";
+import Topbar from "../../components/topbar/Topbar.jsx";
 
 const Profile = () => {
-  return <div>profile page</div>;
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
+        <SidebarLeft />
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img
+                src="/Assets/posts/post3.jpg"
+                alt=""
+                className="profileCoverImage"
+              />
+              <img
+                src="/Assets/profiles/profile5.jpg"
+                alt=""
+                className="profileUserImage"
+              />
+            </div>
+            <div className="profileInfo">
+              <h4 className="profileInfoName">Kristian</h4>
+              <span className="profileInfoDescription">i like beer</span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Feed />
+            <SidebarRight profile />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Profile;
