73a02f3f1044c130d62f76c76cfedfa7928249d4
Kristian Utsar <utsaru@gmail.com>
2022-01-22 20:55:00 +0000

    adds register and login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Home from "./pages/homePage/Home";
+import Login from "./pages/login/Login";
 import Profile from "./pages/profilePage/Profile";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
     <div>
-      {/* <Home /> */}
-      <Profile />
+      <Home />
+      {/* <Profile /> */}
+      {/* <Login /> */}
+      {/* <Register /> */}
     </div>
   );
 }
